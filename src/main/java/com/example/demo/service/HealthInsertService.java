2be6f672483d9781944d1cf9dde5e663ad245b34
한철 <chul.han@biplatform.co.kr>
2024-10-04 11:33:32 +0900

    커밋합니다.

---
+++ src/main/java/com/example/demo/service/HealthInsertService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+//import com.example.demo.mapper.HealthInfoMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class HealthInsertService {
+
+  //  private final HealthInfoMapper healthInfoMapper;
+
+    /*public HealthInfoService(HealthInfoMapper healthInfoMapper) {
+        this.healthInfoMapper = healthInfoMapper;
+    }*/
+
+    public Map<String, Object> getHealthInfo(Long id) {
+        Map<String, Object> a = new HashMap<>();
+        //return healthInfoMapper.selHealthInfo(id);
+        return a;
+    }
+}
